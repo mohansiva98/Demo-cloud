9e37d2a2a034297bd7dc2c8b58b9a55e70402c2a
Mohan Paramasivam <mohanparamasivam@AGLS-MLT-505.local>
2025-07-15 18:44:19 +0530

    test

--- terraform/alb_route53.tf
+++ terraform/alb_route53.tf
@@ -44,25 +44,27 @@ resource "aws_lb" "app_alb" {
 
 # ALB Target Group
 resource "aws_lb_target_group" "app_tg" {
-  name     = "${var.project_name}-tg"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
-   depends_on = [
-    aws_lb_listener.app_listener
-  ]
-
   health_check {
-    path                = "/healthz"
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
 
 # ALB Listener
 resource "aws_lb_listener" "app_listener" {
