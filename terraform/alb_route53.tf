6c051737205085cb69895a6698b98adcb48a165f
Mohan Paramasivam <mohanparamasivam@AGLS-MLT-505.local>
2025-07-15 18:25:55 +0530

    test

--- terraform/alb_route53.tf
+++ terraform/alb_route53.tf
@@ -48,7 +48,7 @@ resource "aws_lb_target_group" "app_tg" {
   port     = 8000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-  target_type = "3.110.166.218"
+  target_type = "ip"
 
   health_check {
     path                = "/healthz"
