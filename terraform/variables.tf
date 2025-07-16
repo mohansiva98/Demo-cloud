3cfb6a22ec65813dbe44b850b3cfc47ad5ec05f1
Mohan Paramasivam <mohanparamasivam@AGLS-MLT-505.local>
2025-07-16 16:36:49 +0530

    test

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "ap-south-1"
-}
-
-variable "project_name" {
-  description = "Name for project prefixing"
-  default     = "cloud-test-demo"
-}
